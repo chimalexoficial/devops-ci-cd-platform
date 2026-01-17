1faa580c63987b8a5a6c2cf2fa90c3831130d32a
chimalexoficial <chimalexoficial@gmail.com>
2026-01-17 14:03:13 -0600

    add test for eksctl

--- scripts/05-eksctl-installation-for-aws.sh
+++ scripts/05-eksctl-installation-for-aws.sh
@@ -11,3 +11,5 @@ curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_ch
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+eksctl version
