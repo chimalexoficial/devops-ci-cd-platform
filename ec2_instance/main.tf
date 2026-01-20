5419acad7b82fbaf0ad1d68440b8f6d509ef6ae6
Arturo Alexis Salgado <chimalexoficial@gmail.com>
2026-01-20 15:33:21 -0600

    Update AWS key name again

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "aws-key.pem" 
+  key_name      = "aws-2026.pem" 
   tags = {
     Name = var.instance_name
   }
