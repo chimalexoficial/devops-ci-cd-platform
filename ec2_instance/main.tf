f0d44f8fd6dc54eb87b9d9c9c56cc389d37b1434
Arturo Alexis Salgado <chimalexoficial@gmail.com>
2026-01-20 15:47:17 -0600

    remove pem extension on AWS key name

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "aws-2026.pem" 
+  key_name      = "aws-2026" 
   tags = {
     Name = var.instance_name
   }
